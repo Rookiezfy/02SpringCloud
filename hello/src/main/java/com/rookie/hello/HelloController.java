0822663803134d317a30358f25143570e9fa742e
rookiezfy <1037957472@qq.com>
2018-07-26 15:23:58 +0800

    eureka client 想注册中心注册一个服务

--- hello/src/main/java/com/rookie/hello/HelloController.java
+++ hello/src/main/java/com/rookie/hello/HelloController.java
@@ -1,7 +1,14 @@
 package com.rookie.hello;
 
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * hello
@@ -12,8 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String index() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello, host:"+ instance.getHost() + ",service_id:"+
+        instance.getServiceId());
         return "Hello World";
     }
 }
